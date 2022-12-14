1a34ee549f0be90f91f4db1f887f57ff55965d41
kulhab <kulhabesh31@gmail.com>
2022-12-14 06:14:31 -0800

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code
+ * @a: single value input
+ * @b: single value input
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int result;
+	result = a+b;
+	return (result);
+}
