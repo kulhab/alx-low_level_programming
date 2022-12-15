359340dbb210d5a083c6b081df973b1e6acef432
kulhab <kulhabesh31@gmail.com>
2022-12-15 09:47:34 -0800

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ * _isupper - check the code.
+ * @c: a single input value
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
