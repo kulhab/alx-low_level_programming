0384996309e10ca88c24b37cc36657d1967b4925
kulhab <kulhabesh31@gmail.com>
2022-12-15 10:03:49 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+ * _isdigit - check the code
+ * @c: single input value
+ * Return: Always 0.
+ */
+int _isdigit(char c)
+{
+	if(c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
