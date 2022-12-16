76e87f9cedd8890846271c98a6da95dac1ad96e3
kulhab <kulhabesh31@gmail.com>
2022-12-15 21:59:33 -0800

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+		for(i = 1; i <= 100; i++)
+		{
+			if(i >=3 && i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if(i >= 5 && i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else if(i >= 5 && i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+			{
+				printf("%d",i);
+			}
+			printf(" ");
+		}
+		return (0);
+}
+
