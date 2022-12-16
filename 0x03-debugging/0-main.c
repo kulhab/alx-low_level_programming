64862c24688e70aa8bb1491a7c0db95b4da978a1
kulhab <kulhabesh31@gmail.com>
2022-12-15 23:23:23 -0800

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+	int i = 0;
+	int r;
+		r = positive_or_negative(i);
+		printf("%d is zero",r);
+
+	return (0);
+}
