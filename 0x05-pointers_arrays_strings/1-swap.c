b7011faf693eaf9bf5ab92cb2c906f455c9a7c6b
kulhab <kulhabesh31@gmail.com>
2022-12-19 08:57:37 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - check the code
+ * @a: pointer variable
+ * @b: pointer variable
+ * @tmp: temp variable of pointer type
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
+}
