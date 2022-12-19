52a45205ef48fa01ac74e447817807bf864e5f12
kulhab <kulhabesh31@gmail.com>
2022-12-19 09:21:19 -0800

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+	i = 0;
+		while (str[i] != 0)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+		_putchar('\n');
+}
