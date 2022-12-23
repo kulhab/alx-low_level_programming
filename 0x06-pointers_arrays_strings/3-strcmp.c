aad06657bef0ae7b0103f16b1429304a54a36876
kulhab <kulhabesh31@gmail.com>
2022-12-22 23:02:18 -0800

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,6 @@
 *  _strcmp - Function which compare two strings and
 *@s1: first string
 *@s2:second string
-*Return:
-*		returns zero if s1 == s2
-*		returns negative number if s1 < s2
-*		returns positive number if s1 > s2
 */
 
 int _strcmp(char *s1, char *s2)
@@ -38,3 +34,4 @@ int _strcmp(char *s1, char *s2)
 
 	}
 	return (diff);
+}
