a797e78baa8c8d63cd8958bd904c83032e757b95
kulhab <kulhabesh31@gmail.com>
2022-12-22 22:52:25 -0800

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,10 +12,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	int i = 0, len = 0;
 
-	while (i[index++])
+	while (dest[i++])
 		len++;
 
-	for (i = 0; src[i] && i < n; i)
+	for (i = 0; src[i] && i < n; i++)
 		dest[len++] = src[i];
 
 	return (dest);
